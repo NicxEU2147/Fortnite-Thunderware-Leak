7bc74e4ecedf13a866d2381e3340e84413a6501b
NicxEU2147 <79059073+NicxEU2147@users.noreply.github.com>
2021-02-14 19:26:59 +0000

    Add files via upload

---
+++ Thunderware-Leak-Main/imgui/Settings.h
@@ -0,0 +1,24 @@
+#pragma once
+
+namespace Settings
+{
+
+	INT Options;
+
+	bool aimbot;
+	bool fortnite1;
+	bool fortnite2;
+	bool csgomoment;
+
+	float fortniteslider = 100;
+	float smooth = 10;
+	float distance = 300;
+	int bone = 5;
+	int type = 3;
+	int silent_accuracy = 100;
+	float circle = 150.f;
+
+	// Literally just settings for checkboxes :shrug:
+
+	bool begin;
+}
